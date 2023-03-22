6ea171c591aab43e94bafc55822cb3adabb24d74
Devmanuel01 <devmanuel01@gmail.com>
2023-03-22 20:12:58 +0000

    Edited the program to align with betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,20 +5,27 @@ void printNewChar(char *m);
  * main - check the code
  *
  * Return: Always 0.
+ *
  */
 int main(void)
 {
 	char s[] = "_putchar";
-	
+
 	printNewChar(s);
 
 	return (0);
 }
 
-void printNewChar (char *m)
+/**
+ * printNewChar - Runs through an array of char and prints out each char
+ * @m: The string of char to run through
+ *
+ * _putchar: prints the char to stdout
+ */
+void printNewChar(char *m)
 {
 	int i = 0;
-	
+
 	while (m[i] != '\0')
 	{
 		_putchar(m[i]);
