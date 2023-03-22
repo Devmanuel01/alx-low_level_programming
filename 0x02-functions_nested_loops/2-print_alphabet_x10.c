8ea5cbecf95d0c9ead73f29f77ebf02c61144079
Devmanuel01 <devmanuel01@gmail.com>
2023-03-22 21:27:17 +0000

    prints the small letters of the alphabet to the stdout 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the small letters of alphabets 10x
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		print_alphabet();
+		i++;
+	}
+}
