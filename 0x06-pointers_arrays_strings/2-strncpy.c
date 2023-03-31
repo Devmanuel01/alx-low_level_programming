9b70d44e9c7de61c0b390100c5d20245b463f70b
Devmanuel01 <devmanuel01@gmail.com>
2023-03-31 00:11:33 +0000

    This function copies a string.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -25,13 +25,14 @@ char *_strncpy(char *dest, char *src, int n)
 			dest[a] = src[a];
 			a++;
 		}
+		dest[a] = '\0';
 	}
 	else
 	{
 		while (a < n)
 		{
 			dest[a] = src[a];
-			a++
+			a++;
 		}
 	}
 	return (dest);
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-	char s1[98] = "Hello ";
-	char s2[] = "World!\n";
+	char s1[98];
 	char *ptr;
+	int i;
 
+	for (i = 0; i < 98 -1; i++)
+	{
+		s1[i] = '*';
+	}
+	s1[i] = '\0';
 	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strncat(s1, s2, 1);
-	printf("%s\n", s1);
-	printf("%s", s2);
-	printf("%s\n", ptr);
-	ptr = _strncat(s1, s2, 3);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
 	printf("%s\n", s1);
 	printf("%s\n", ptr);
 	return (0);
